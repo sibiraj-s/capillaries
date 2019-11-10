32c4c6bfde7559989f6ca1b399b7f48dc0503770
sibiraj-s <dev.sibiraj@outlook.com>
2019-11-10 19:34:20 +0530

    chore: jest ignore dist directory

--- jest.config.js
+++ jest.config.js
@@ -85,7 +85,9 @@ module.exports = {
 
   // An array of regexp pattern strings,
   // matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/'
+  ],
 
   // Activates notifications for test results
   // notify: false,
